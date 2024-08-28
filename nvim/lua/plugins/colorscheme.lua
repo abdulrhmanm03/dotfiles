684f434af2a13d072907f7b328e22efacb4711aa
abdulrhman <abdulrhmanhegazy2003@gmail.com>
2024-08-28 21:43:44 +0300

    in sway update the wallpaper and the browset keymap, change the look of waybar and kitty theme, add mini.ai pugin to neovim and change the colorscheme and some keymaps

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,26 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end,
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			theme = "dragon",
+			background = { -- map the value of 'background' option to a theme
+				dark = "dragon", -- try "dragon" !
+				light = "lotus",
+			},
+			globalStatus = true,
+			colors = {
+				theme = { all = { ui = { bg_gutter = "none" } } },
+			},
+		})
+		vim.cmd.colorscheme("kanagawa")
+	end,
+
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
+	-- priority = 1000,
+	-- config = function()
+	--     vim.cmd.colorscheme "catppuccin-mocha"
+	-- end,
 }
